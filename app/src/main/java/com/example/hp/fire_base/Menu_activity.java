324e0de71bf3b77b8885859ef59c0bfc73dd630c
hp <ferdowsjust16@gmail.com>
2019-05-04 01:04:42 +0600

    update

--- app/src/main/java/com/example/hp/fire_base/Menu_activity.java
+++ app/src/main/java/com/example/hp/fire_base/Menu_activity.java
@@ -1,11 +1,20 @@
 package com.example.hp.fire_base;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Menu_activity extends AppCompatActivity {
+
+    private ImageButton imageButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +22,15 @@ public class Menu_activity extends AppCompatActivity {
         setContentView(R.layout.activity_menu_activity);
 
         this.setTitle("Menu Activity" );
+
+        imageButton = (ImageButton) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
